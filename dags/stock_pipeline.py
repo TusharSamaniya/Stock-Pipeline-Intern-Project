acf02546cb08d930fba40e0eccab8c09b89dbe8d
Tusha Samaniya <tusharsamaniya02@gmail.com>
2026-09-10 16:11:14 +0530

    Task 2: Create DAG File

---
+++ dags/stock_pipeline.py
@@ -0,0 +1,44 @@
+import sys
+import os
+from datetime import datetime
+
+# Add /app to sys.path to allow importing modules from the /app/scripts directory
+sys.path.append('/app')
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+# Import our pipeline functions
+from scripts.fetch_data import fetch_stock_data
+from scripts.transform_data import transform_stock_data
+from scripts.load_data import load_data_to_db
+
+with DAG(
+    dag_id="stock_pipeline",
+    start_date=datetime(2026, 9, 1),
+    schedule_interval="@daily",
+    catchup=False,
+    tags=["stock_market"]
+) as dag:
+
+    # Task 1: Fetch
+    fetch_task = PythonOperator(
+        task_id="fetch_data",
+        python_callable=fetch_stock_data,
+        op_kwargs={"symbol": "AAPL"}
+    )
+
+    # Task 2: Transform
+    transform_task = PythonOperator(
+        task_id="transform_data",
+        python_callable=transform_stock_data
+    )
+
+    # Task 3: Load
+    load_task = PythonOperator(
+        task_id="load_data",
+        python_callable=load_data_to_db
+    )
+
+    # Set dependencies
+    fetch_task >> transform_task >> load_task
